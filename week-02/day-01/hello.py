1c0538b7793160d0c5b262ec46075f61261cecf4
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-11 12:22:19 +0200

    feat(day-01) Hello.me and Humpty_dumpty exercised are completed

---
+++ week-02/day-01/hello.py
@@ -0,0 +1,13 @@
+print('hello world!')
+student_count = 214
+student_per_class = 22
+is_class_ready = True
+
+if is_class_ready:
+    print( student_count + student_per_class )
+else:
+    print('we need\' practice')
+
+
+for i in range(3):
+    print(i)
