d3d4358893f7c312f64523bbc61b5bb906c48c81
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-11 15:45:37 +0200

    feat(day-01) hello_user.py are completed.

--- week-02/day-01/hello_user.py
+++ week-02/day-01/hello_user.py
@@ -1,3 +1,6 @@
 # Modify this program to greet the user instead of the World!
 # The program should ask for the name of the user
-print("Hello, World!")
+# print("Hello, World!")
+
+user_input = input('What is your name?')
+print("Hello, " + user_input)
