ef4a1841e04bcd8779e9d274d9981cba24f056a4
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-11 14:51:23 +0200

    feat(day-01) define_basic_info.py are completed.

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,11 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+name = "Tamara"
+age = int(31)
+height = 1.66 * 2
+marriage = True
+print(name)
+print(age)
+print(height)
+print(marriage)
