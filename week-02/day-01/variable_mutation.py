dea911cdbfd64a815478f33455e2a733e77e7dfe
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-11 15:21:16 +0200

    feat(day-01) variable_mutation.py are completed.

--- week-02/day-01/variable_mutation.py
+++ week-02/day-01/variable_mutation.py
@@ -1,6 +1,6 @@
 a = 3
 # make it bigger by 10
-
+a += 10
 
 print(a)
 
@@ -9,7 +9,7 @@ print(a)
 
 b = 100
 # make it smaller by 7
-
+b -= 7
 
 print(b)
 
@@ -19,7 +19,7 @@ print(b)
 c = 44
 # please double c's value
 
-
+c *= 2
 print(c)
 
 
@@ -27,7 +27,7 @@ print(c)
 
 d = 125
 # please divide by 5 d's value 
-
+d /= 5
 
 print(d)
 
@@ -36,7 +36,7 @@ print(d)
 
 e = 8
 # please cube of e's value
-
+e ** 3
 
 print(e)
 
@@ -46,7 +46,10 @@ print(e)
 f1 = 123
 f2 = 345
 # tell if f1 is bigger than f2 (pras a boolean)
-
+if f1 > f2:
+    print (True)
+else:
+    print(False)
 
 
 
@@ -55,12 +58,18 @@ g1 = 350
 g2 = 200
 # tell if the double of g2 is bigger than g1 (pras a boolean)
 
-
+if g2 * 2 > g1:
+    print(True)
+else:
+    print(False)
 
 
 h = 1357988018575474
 # tell if it has 11 as a divisor (pras a boolean)
-
+if h % 11 == 0:
+    print(True)
+else:
+    print(False)
 
 
 
@@ -68,18 +77,30 @@ h = 1357988018575474
 i1 = 10
 i2 = 3
 # tell if i1 is higher than i2 squared and smaller than i2 cubed (pras a boolean)
+if i2 ** 3 > i1 > i2**2:
+    print(True)
+else:
+    print(False)
+
+if i1 > i2**2 and i1 < i2 ** 3:
+    print(True)
+else:
+    print(False)
 
 
 
 
 j = 1521
 # tell if j is dividable by 3 or 5 (pras a boolean)
-
+if j % 3 == 0 or j % 5 == 0:
+    print(True)
+else:
+    print(False)
 
 
 
 k = "Apple"
 #fill the k variable with its cotnent 4 times
-
+k *= 4
 
 print(k)
