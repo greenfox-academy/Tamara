e1383c69552b15cca15c9a42270dbea8c412fbe4
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-11 13:33:46 +0200

    feat(day01) Two_numbers.py is completed.

--- week-02/day-01/two_numbers.py
+++ week-02/day-01/two_numbers.py
@@ -11,4 +11,9 @@
 # Print the integer part of 22 divided by 13
 
 # Print the reminder of 22 divided by 13
-
+print(13 + 22)
+print(22 - 13)
+print(13 * 22)
+print(22 / 13)
+print(22 // 13)
+print(22 % 13)
