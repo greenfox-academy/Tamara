e7a6ca2a270197e3d3c1e2111401b8a50e1dcb9e
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-12 11:53:01 +0200

    feat(week02_day-02) append_a.py is completed.

---
+++ week-02/day-02/append_a.py
@@ -0,0 +1,12 @@
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
+
+am = 'kuty'
+def appendA(string):
+    string=['a']
+    string.append(am)
+    return string
+
+print(appendA(am))
