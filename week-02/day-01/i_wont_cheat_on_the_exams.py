e7286fc9a4b8db5d16f4fd5534e3b8cad52b8483
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-12 22:50:48 +0200

    feat(week02_day-01): i_wont_cheat_on_the_exams.py and Print_even.py are completed.

--- week-02/day-01/i_wont_cheat_on_the_exams.py
+++ week-02/day-01/i_wont_cheat_on_the_exams.py
@@ -1,2 +1,7 @@
 # Create a program that writes this line 100 times:
 # "I won't cheat on the exam!"
+a = 0
+while a <= 100:
+    print("I won't cheat on the exams!")
+    a +=1
+    
--- week-02/day-01/print_even.py
+++ week-02/day-01/print_even.py
@@ -1,1 +1,7 @@
 # Create a program that prints all the even numbers between 0 and 500
+
+a = 0
+for a in range(0, 500):
+    if a % 2 == 0:
+        print(a)
+        a += 1
