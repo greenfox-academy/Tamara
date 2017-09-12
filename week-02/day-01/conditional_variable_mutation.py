3c16eeb2e07c3b5241244d8511d2d2dcfe85fad3
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-12 22:38:04 +0200

    feat(week02_day-01): conditional_variable_mutation.py is completed.

--- week-02/day-01/conditional_variable_mutation.py
+++ week-02/day-01/conditional_variable_mutation.py
@@ -1,41 +1,48 @@
 a = 24
 out = 0
 # if w is even increment out by one
-
-
+if a % 2 == 0:
+    out += 1
 print(out)
 
 
-
-
-b = 13
-out2 = ""
 # if b is between 10 and 20 set out2 to "Sweet!"
 # if less than 10 set out2 to "More!",
 # if more than 20 set out2 to "Less!"
-
-
+b = 13
+out2 = ""
+if 10 < b <= 20:
+    out2 = ("Sweet")
+elif b < 10:
+    out2 = "More!"
+else:
+    out2 = ("Less!")
+  
 print(out2)
 
 
 
 c = 123
 credits = 100
-is_bonus = False
+is_bonus = True
 # if credits are at least 50,
 # and is_bonus is false decrement c by 2
 # if credits are smaller than 50,
 # and is_bonus is false decrement c by 1
 # if is_bonus is true c should remain the same
-
+if is_bonus == False:
+    if credits >= 50:
+        c -= 2
+    else:
+        c -= 1
 
 print(c)
 
 
 
 
-d = 8
-time = 120
+d = 9
+time = 201
 out3 = ""
 # if d is dividable by 4
 # and time is not more than 200
@@ -43,6 +50,12 @@ out3 = ""
 # if time is more than 200
 # set out3 to "Time out"
 # otherwise set out3 to "Run Forest Run!"
-
-
+if time <= 200:
+    d % 4 == 0
+    out3 = "check"
+elif d % 4 != 0:
+    out3 = "Run Forest Run!" 
+else:
+    out3 = "Time out"
+    
 print(out3)
