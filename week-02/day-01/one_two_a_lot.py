04ca408894d33f103b87df6a9a6d5bb3f29c23e3
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-12 07:04:00 +0200

    feat(week2-day-01) one_day_a_lot.py is completed.

--- week-02/day-01/one_two_a_lot.py
+++ week-02/day-01/one_two_a_lot.py
@@ -3,3 +3,13 @@
 # If the number is one it should print: One
 # If the number is two it should print: Two
 # If the number is more than two it should print: A lot
+
+i = input ("Add a number: ")
+if int(i) == 0 or int(i) < 0:
+    print("Not enough")
+elif int(i) == 1:
+    print("One")
+elif int(i) == 2:
+    print("Two")
+elif int(i) > 2:
+    print("A lot")
