349ad1533f171c6bf4c4439ccc4d65ee97321219
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-13 16:19:22 +0200

    feat(week02_day03): 02 exercises is completed in dictionary session.

---
+++ week-02/day-03/dictionary_02.py
@@ -0,0 +1,29 @@
+
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+
+# create a function that takes a list of students and prints:
+# - Who has got more candies than 4 candies
+
+# create a function that takes a list of students and prints: 
+#  - how many candies they have on average
+
+def name_counter(students):
+    names = 0
+    for student in students:
+        if student['candies'] > 4:
+            names = student['name']
+    return names
+print(name_counter(students))
+
+average_candies = 0
+def average():
+    avg = 0
+    for i in range(len(students)):
+        avg += students[i]['candies']/len(students)
+    print(avg)
+#print(average())
