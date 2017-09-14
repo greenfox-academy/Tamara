2eb8c3f5a81b4ba7ecb0734392a84bbb125a9a27
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-14 16:51:16 +0200

    feat(week02_day01): draw_pyramid.py is completed.

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -8,3 +8,16 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+
+N,line,c,p = 4,'','',1
+spaces = ' '*N
+
+for i in range(N):
+  spaces = spaces[0:N-i]
+  c = ''
+  for j in range(p):
+    c += "*"
+  line += spaces+c+'\n'
+  p += 2
+
+print(line)
