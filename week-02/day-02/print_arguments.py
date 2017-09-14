5071a73abea89981b6c3fd8cedb0869f45b00a6c
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-14 09:58:20 +0200

    feat(week02_day02): print_arguments.py is completed.

--- week-02/day-02/print_arguments.py
+++ week-02/day-02/print_arguments.py
@@ -2,9 +2,9 @@
 #   which prints the input parameters
 #   (can have multiple number of arguments)
 
-from print import argument
-def printer(argument):
-    input = (argument)
-   
-print(printer(arg))
-print("Total score for " + name + " is " + score)
+import sys
+argv = ['hello', 'world', 'arg3', 'arg4']
+
+def printer(argv):
+    return argv
+print (argv[0:])
