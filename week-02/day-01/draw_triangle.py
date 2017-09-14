722f9f543431800c3257413100b2186d302fee81
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-14 16:09:43 +0200

    feat(week02_day01): draw_triangle.py is completed.

--- week-02/day-01/draw_triangle.py
+++ week-02/day-01/draw_triangle.py
@@ -7,3 +7,31 @@
 # ****
 #
 # The triangle should have as many lines as the number was
+
+number = int(input("Write a number! "))
+for i in range(number):
+    print("*" * (i + 1))
+
+# with list:
+# stars = 0
+# def triangle(stars):
+#     star = []
+#     size = 4
+#     for i in range(size):
+#         row = []
+#         for j in range(size):
+#             if j == i:
+#                  row.append("*")
+#             elif j < i:
+#                 row.append("*")
+#             else:
+#                 row.append(" ")
+#         star.append(row)
+
+#     for triangle_row in range(len(star)):
+#         print(star[triangle_row])
+
+# triangle(stars)
+
+
+        
