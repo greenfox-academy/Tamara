7dd63b4393c948cb34a9798f961d31246c9494be
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-14 10:56:09 +0200

    feat(week02_day02): compare_length.py is completed.

--- week-02/day-02/compare_length.py
+++ week-02/day-02/compare_length.py
@@ -5,13 +5,20 @@
 # - Print if `p2` has more elements than `p1`
 p1 = [1, 2, 3]
 p2 = [4, 5]
-def lens (k):     
-    i = len(p1)
-    j = len(p2) 
-    k = j - i
-    if  k > 1:
-        print(p2[0:])
-    elif k < 1:
-        p2.append(i)
-        k = j - i
-        return k
+i = len(p1)
+j = len(p2) 
+k = j - i  
+#def lens (k):  
+#   while k < 1:
+#      p2.append(p1[1])
+#      return k
+#   else:
+#       print(len(k))
+#lens(k)
+
+def lens(k):
+    if j < i:
+       print('less')
+    else:
+       print('more')
+lens(k)
