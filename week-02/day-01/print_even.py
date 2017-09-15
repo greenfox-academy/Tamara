91ea093ec8b51b8977b69fb0733bd0c5f28f7de5
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-15 10:00:21 +0200

    feat(week02_day01): creating functions incount_from_to.py, draw_diamond.py, print_even.py

--- week-02/day-01/print_even.py
+++ week-02/day-01/print_even.py
@@ -1,7 +1,7 @@
 # Create a program that prints all the even numbers between 0 and 500
 
 a = 0
-for a in range(0, 500):
+for a in range(0, 501):
     if a % 2 == 0:
         print(a)
         a += 1
