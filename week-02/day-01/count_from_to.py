a3a0fb6347ec696d969c29503f4897a0711947a3
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-17 22:11:23 +0200

    feat(week02-day01): count_from_to.py is completed.

--- week-02/day-01/count_from_to.py
+++ week-02/day-01/count_from_to.py
@@ -11,3 +11,17 @@
 # 3
 # 4
 # 5
+
+#user_input = ("Please, give a number!" )
+#user_input = ("Please, give another number!" )
+
+x = int(input("Please, give a number!" ))
+y = int(input("Please, give another number!" ))
+def counter(x, y):
+    if x > y:
+        print("The second number should be bigger")
+    else:
+        while x < y:
+            print(x)
+            x += 1       
+counter(x, y)
