8ae511088a8606c95bd87e8b7046d866236277f9
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-17 20:18:21 +0200

    feat(week02-day01): draw_diamond.py gets new feature.

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,14 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+N = int(input("Give me a number! "))
+stars = 0
+for i in range(N):
+    if i < N/2:
+        stars = i * 2 + 1
+    else:
+        stars = ((N - 1 - i) * 2 + 1)
+    spaces = int((N - stars)/2)
+    print(' ' * spaces + '*' * stars + ' ' * spaces)
+
+
