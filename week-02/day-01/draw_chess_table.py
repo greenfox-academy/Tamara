d34c9c1ac7ad9f9114356b78860fcea9798f17f9
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-17 21:37:28 +0200

    feat(week02-day01): draw_chess_table.py is completed.

--- week-02/day-01/draw_chess_table.py
+++ week-02/day-01/draw_chess_table.py
@@ -9,3 +9,13 @@
 # % % % % 
 #  % % % %
 #
+N = 8
+
+def chess_table():
+    for i in range(N):
+        if i % 2 == 0:
+            print(int(N/2) * "% ")
+        else:
+            print(int(N/2) * " %")
+
+chess_table()
