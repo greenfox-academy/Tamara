5c425f9f3b5e418b2c6dce81ca9ac88e876c1969
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-18 12:09:24 +0200

    feat(week03_day01): count_lines.py is completed.

---
+++ week-03/day-01/count_lines.py
@@ -0,0 +1,16 @@
+# Write a function that takes a filename as string,
+# then returns the number of lines the file contains.
+# It should return zero if it can't open the file, and
+# should not raise any error.
+
+file_name = input("Give me a filename! ")
+def count_lines(file_name):
+    try:
+        lines = 0
+        for line in open("file_name"):
+            lines += 1
+        return lines
+    except FileNotFoundError:
+        return 0
+
+print(count_lines(file_name))
