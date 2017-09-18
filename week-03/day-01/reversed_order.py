702d976069f918cfdf91f47225b55c7b594c83b8
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-18 18:18:49 +0200

    feat(week03_day01): reversed_order.py is completed

---
+++ week-03/day-01/reversed_order.py
@@ -0,0 +1,11 @@
+# Create a method that decrypts reversed-order.txt
+
+
+def decrypt(file_name):
+    with open(file_name, "r") as f:
+        text = f.read()
+        reversed = text[::-1]
+    print(reversed)
+
+    f.close()
+decrypt("reversed_order.txt")
