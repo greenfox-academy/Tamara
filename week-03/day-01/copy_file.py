1b9075b126be0e07cdce229148444d235d561628
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-18 16:45:08 +0200

    feat(week03_day01): copy_file.py is completed.

---
+++ week-03/day-01/copy_file.py
@@ -0,0 +1,16 @@
+# Write a function that copies a file to an other
+# It should take the filenames as parameters
+# It should return a boolean that shows if the copy was successful
+
+
+def copy_file(in_file, out_file):
+    in_file = open(in_file, "r")
+    out_file = open(out_file, "w")
+    try:
+        for i in in_file:
+            out_file.write(i)
+            return True
+    except:
+        print("Something is wrong.")
+    
+copy_file("my-file.txt", "out_file.txt")    
