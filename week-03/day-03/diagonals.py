7745e966ebde279cf065a794b8f61645d5b9e9e2
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 12:23:37 +0200

    feat(week03_day03): creating diagonals.

--- week-03/day-03/diagonals.py
+++ week-03/day-03/diagonals.py
@@ -6,5 +6,9 @@ canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
 # draw the canvas' diagonals in green.
+green_line = canvas.create_line(0, 300, 300, 0, fill='green')
+green_line = canvas.create_line(0, 0, 300, 300, fill='green')
+
+
 
 root.mainloop()
