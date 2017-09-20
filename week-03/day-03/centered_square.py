f2df530f77d28f695b7724318a2f7411afc83e1f
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 13:23:42 +0200

    feat(week03_day03): creating centered 10x10 box.

--- week-03/day-03/centered_square.py
+++ week-03/day-03/centered_square.py
@@ -6,5 +6,6 @@ canvas = Canvas(root, width='300', height='300')
 canvas.pack()
 
 # draw a green 10x10 square to the center of the canvas.
+green_box = canvas.create_rectangle(140, 140, 150, 150, fill='green')
 
 root.mainloop()
