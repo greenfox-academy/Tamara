bdfd29173d7ababe3bff6ed45746d8f1c37ad339
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 12:39:48 +0200

    feat(week03_day03): creating horizontal lines from one point.

--- week-03/day-03/horizontal_lines.py
+++ week-03/day-03/horizontal_lines.py
@@ -10,4 +10,12 @@ canvas.pack()
 # and draws a 50 long horizontal line from that point.
 # draw 3 lines with that function.
 
+def horizontal_lines_drawing(x, y):
+    blue_line = canvas.create_line(x, y, 50, 50, fill='blue')
+    red_line = canvas.create_line(x, y, 50, 40, fill='red')
+    green_line = canvas.create_line(x, y, 50, 60, fill='green')
+
+
+horizontal_lines_drawing(0, 50)
+
 root.mainloop()
