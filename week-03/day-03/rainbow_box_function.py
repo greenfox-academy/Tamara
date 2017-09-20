3a14d5a69d84d4c7112e58482012659fe6bd61d6
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 15:12:34 +0200

    feat(week03_day03): creating rainbow squares.

--- week-03/day-03/rainbow_box_function.py
+++ week-03/day-03/rainbow_box_function.py
@@ -10,4 +10,14 @@ canvas.pack()
 # and draws a square of that size and color to the center of the canvas.
 # create a loop that fills the canvas with rainbow colored squares.
 
+rainbow = ('red', 'orange', 'yellow', 'green','blue','indigo','violet')
+def rainbow_squares (size, color):
+    canvas.create_rectangle(150 - size, 150 - size, 150 + size, 150 + size, 
+                                       fill=color)
+        
+size = 150
+for i in rainbow:
+    rainbow_squares(size, i)
+    size -= 20
+
 root.mainloop()
