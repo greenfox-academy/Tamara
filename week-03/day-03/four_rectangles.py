d92907dd26f6cc903e6672e47867497b5a20f898
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 13:31:49 +0200

    feat(week03_day03): creating 4 rectangles with function.

--- week-03/day-03/four_rectangles.py
+++ week-03/day-03/four_rectangles.py
@@ -7,4 +7,12 @@ canvas.pack()
 
 # draw four different size and color rectangles.
 
+def four_rectangles():
+    blue_box = canvas.create_rectangle(120, 120, 170, 170, fill='blue')
+    red_box = canvas.create_rectangle(90, 90, 100, 100, fill='red')
+    orange_box = canvas.create_rectangle(200, 150, 150, 190, fill='orange')
+    green_box = canvas.create_rectangle(80, 40, 40, 90, fill='green')
+
+
+four_rectangles()
 root.mainloop()
