201229bf703374b74deabefc62d3699e543d9cc0
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 16:07:55 +0200

    feat(week03_day03): creating purple steps in a diagonal line.

--- week-03/day-03/purple_steps.py
+++ week-03/day-03/purple_steps.py
@@ -8,4 +8,14 @@ canvas.pack()
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
 
+x = 0
+y = 10
+def purple_steps_squares(x, y):
+    for i in range(1, 15):
+        canvas.create_rectangle(x, x, y, y, fill='purple')
+        x += 10 
+        y += 10
+    
+
+purple_steps_squares(x, y)
 root.mainloop()
