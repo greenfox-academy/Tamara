c9e536aa1c60524758b33ea7dcbec580a2500f40
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-20 12:33:34 +0200

    feat(week03_day03): creating lines from one point.

--- week-03/day-03/go_to_center.py
+++ week-03/day-03/go_to_center.py
@@ -10,4 +10,12 @@ canvas.pack()
 # and draws a line from that point to the center of the canvas.
 # draw 3 lines with that function.
 
+def three_lines_drawing(x, y):
+    green_line = canvas.create_line(x, y, 150, 150, fill='green')
+    red_line = canvas.create_line(x, y, 140, 120, fill='red')
+    red_line = canvas.create_line(x, y, 140, 170, fill='red')
+
+
+
+three_lines_drawing(0, 0)
 root.mainloop()
