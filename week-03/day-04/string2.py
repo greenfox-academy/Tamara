3e31403a7f47293167e77e691f87e2bf311202a7
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-21 15:42:02 +0200

    feat(string2) creating a recursive feature

--- week-03/day-04/string2.py
+++ week-03/day-04/string2.py
@@ -1,1 +1,11 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+def recursive_remove_char(word):
+    if word == "":
+        return word
+    elif word[0] == "x":
+        return "" +  recursive_remove_char(word[1:])
+    else:
+        return word[0] + recursive_remove_char(word[1:])
+
+print(recursive_remove_char("Relax"))
