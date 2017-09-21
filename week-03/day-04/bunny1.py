1201dbd82aeca94e68adf27dcd9e02e5c382b7ca
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-21 12:58:11 +0200

    feat(bunny1) creating a recursive feature

--- week-03/day-04/bunny1.py
+++ week-03/day-04/bunny1.py
@@ -1,2 +1,11 @@
 # We have a number of bunnies and each bunny has two big floppy ears.
 # We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+def sum_bunnies_ears(n):
+    if n == 0:
+        return 0
+    else:
+        return 2 + sum_bunnies_ears(n - 1)
+
+
+print(sum_bunnies_ears(5))
