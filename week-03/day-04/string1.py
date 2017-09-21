740a78982bdcf395c9ee0e5a610a797fec595c95
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-21 15:40:58 +0200

    feat(string1) creating a recursive feature

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,2 +1,14 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+def recursive_string(word):
+    if word == "":
+        return word
+    elif word[0] == "x":
+        return "y" +  recursive_string(word[1:])
+    else:
+        return word[0] + recursive_string(word[1:])
+
+print(recursive_string("Rexax"))
+
+
