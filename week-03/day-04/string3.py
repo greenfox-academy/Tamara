bf9d6eb516cb61542083b8afd8c20d8d73cca55d
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-21 15:42:12 +0200

    feat(string3) creating a recursive feature

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,10 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+def recursive_string_separating(word):
+    if len(word) == 1:
+        return word
+    else:
+        return word[0] + "*" + recursive_string_separating(word[1:])
+
+print(recursive_string_separating("Relax"))
