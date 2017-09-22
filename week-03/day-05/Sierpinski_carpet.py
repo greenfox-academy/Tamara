f4e7b75e108b5509f4ad674b6f3f40366c5208e6
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-22 14:23:51 +0200

    refact(siepinski_carpes): giving some new option

--- week-03/day-05/Sierpinski_carpet.py
+++ week-03/day-05/Sierpinski_carpet.py
@@ -1,27 +1,27 @@
 from tkinter import *
 import math
-import time
+#import time
 root = Tk()
 
 
-canvas = Canvas(root, width='900', height='900')
+canvas = Canvas(root, width='900', height='900', background='white')
 canvas.pack()
 
 
 def drawing_smaller_squeares(x, y, size):
-    time.sleep(0.01)
-    canvas.update()
-    canvas.create_rectangle(x, y, x+size, y+size, outline='black', fill='white')
+    # time.sleep(0.01)
+    # canvas.update()
+    canvas.create_rectangle(x, y, x+size, y+size, outline='white', fill='black')
 
 
 def draw_fractal(x, y, size):
 
     drawing_smaller_squeares(x, y, size)
-    if size < 10:
+    if size < 3:
         return
     else:
         draw_fractal(x, y, size/3)
-        draw_fractal(x+ size/3, y, size/3)
+        draw_fractal(x+ size/3, y, size/3)     
         draw_fractal(x+2*(size/3), y, size/3)
         draw_fractal(x+2*(size/3), y+size/3, size/3)
         draw_fractal(x+2*(size/3), y+2*(size/3), size/3)
