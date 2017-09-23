fa521d413570b0698295f5c2c20338e094c07b86
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-23 22:59:43 +0200

    feat(line_play): creating lines with function

--- week-03/day-03/line_play.py
+++ week-03/day-03/line_play.py
@@ -8,4 +8,12 @@ canvas.pack()
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
 
+def draw_nice_pic(step, color1, color2):
+    for x in range(step, 299, step):
+        canvas.create_line(x, 0, 300, x, fill=color1)
+        canvas.create_line(0, x, x, 300, fill=color2)
+    
+draw_nice_pic(20, 'purple', 'green')
+
+
 root.mainloop()
