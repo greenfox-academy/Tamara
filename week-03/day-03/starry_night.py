36080b2d8a17a016df596f4bdc9f30d684ec52f4
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-24 19:15:42 +0200

    feat(starry_night): creating small squares like stars with function

--- week-03/day-03/starry_night.py
+++ week-03/day-03/starry_night.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from random import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+
+canvas = Canvas(root, width='300', height='300', background="black")
 canvas.pack()
 
 # draw the night sky:
@@ -10,5 +12,15 @@ canvas.pack()
 # - The stars should be small squares
 # - The stars should have random positions on the canvas
 # - The stars should have random color (some shade of grey)
+
+def night_sky(N):
+    for x in range(N):
+        x = randint(5, 300)
+        y = randint(5, 300)
+        color = choice(['lightgrey', 'grey', 'silver', 'lightslategrey'])
+        canvas.create_rectangle(x, y, x+5, y+5, fill=color)
+    
+
+night_sky(100)
 
 root.mainloop()
