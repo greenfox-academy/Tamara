9b39b515a603f97528740dc6676f6fe9ec64c063
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-24 10:12:45 +0200

    feat(envelope_star): creating star with lines with function

--- week-03/day-03/envelope_star.py
+++ week-03/day-03/envelope_star.py
@@ -8,4 +8,14 @@ canvas.pack()
 # reproduce this:
 # [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
 
+def draw_nice_pic(step, width, color):
+    half = int(width / 2)
+    for x in range(step, half, step):
+        canvas.create_line(x, half, half, half-x, fill=color)
+        canvas.create_line(x, half, half, x+half, fill=color)
+        canvas.create_line(half, x, x+half, half, fill=color)
+        canvas.create_line(x+half, half, half, width-x, fill=color)
+    
+draw_nice_pic(10, 300, 'green')
+
 root.mainloop()
