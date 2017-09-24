5ce02abce85deece6304e13b9d57dd7bd3ac4aca
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-24 10:40:55 +0200

    refact(line_play_quarters): refact to useable function

--- week-03/day-03/line_play_quarters.py
+++ week-03/day-03/line_play_quarters.py
@@ -13,36 +13,24 @@ def line_in_the_middle():
     canvas.create_line(150, 0, 150, 300, fill='black')
 
 
-def draw_nice_pic(step, start, end, color1, color2):
-    for x in range(start, end, step):
-        canvas.create_line(x, start, end, x, fill=color1)
-        canvas.create_line(start, x, x, end, fill=color2)
-color1='purple'
-color2='green'
-#q1
-for x in range(0, 150, 10):
-        canvas.create_line(x, 0, 150, x, fill=color1)
-        canvas.create_line(0, x, x, 150, fill=color2)
-#q2
-for x in range(0, 150, 10):
-        canvas.create_line(x+150, 0, 300, x, fill=color1)
-        canvas.create_line(150, x, x+150, 150, fill=color2)
+def lines_in_quarters(start, width, color1, color2):
+    half = int(width / 2)
+    for x in range(start, half, 10):
+        #q1
+        canvas.create_line(x, start, half, x, fill=color1)
+        canvas.create_line(start, x, x, half, fill=color2)
+        #q2
+        canvas.create_line(x+half, start, width, x, fill=color1)
+        canvas.create_line(half, x, x+half, half, fill=color2)
+        #q3
+        canvas.create_line(x, half, half, x+half, fill=color1)
+        canvas.create_line(start, x+half, x, width, fill=color2)
+        #q4
+        canvas.create_line(x+half, half, width, x+half, fill=color1)
+        canvas.create_line(half, x+half, x+half, width, fill=color2)
 
-#q3
-for x in range(0, 150, 10):
-        canvas.create_line(x, 150, 150, x+150, fill=color1)
-        canvas.create_line(0, x+150, x, 300, fill=color2)
-
-#q4
-for x in range(0, 150, 10):
-        canvas.create_line(x+150, 150, 300, x+150, fill=color1)
-        canvas.create_line(150, x+150, x+150, 300, fill=color2)
 
 line_in_the_middle()
-# draw_nice_pic(20, 0, 150, 'purple', 'green')
-# draw_nice_pic(20, 150, 300, 'purple', 'green')
-# draw_nice_pic(20, 150, 150, 'purple', 'green')
-# draw_nice_pic(20, 300, 300, 'purple', 'green')
-
+lines_in_quarters(0, 300, 'purple', 'green')
 
 root.mainloop()
