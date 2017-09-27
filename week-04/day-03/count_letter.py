f86227395cfffea134a0d1efc75865cc1c9995f4
tamara-0527 <szigeti.tamara@gmail.com>
2017-09-27 21:55:12 +0200

    feat(count_letter): giving test programs

---
+++ week-04/day-03/count_letter.py
@@ -0,0 +1,12 @@
+# # Count Letters
+#  - Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+#  - Create a test for that.
+
+def letter_counter(word):
+    counted_letters = {}
+    for letter in word:
+        if letter in counted_letters:
+            counted_letters[letter] += 1
+        else:
+            counted_letters[letter] = 1
+    return counted_letters
