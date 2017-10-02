f3aef81dea9882f39ca8ba599f1030304385f3b1
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-02 17:33:10 +0200

    feat(rpg_game): hero is moving

--- week-05/day-01/view.py
+++ week-05/day-01/view.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
 from map import Map
+from entity import *
 
 size = 720
 root = Tk()
@@ -29,11 +30,30 @@ class DrawEntity:
         self.drawing_entity()
 
     def drawing_entity(self):
-        canvas.create_image(0, 0, anchor = NW, image = self.hero)
+        self.entity_img = canvas.create_image(0, 0, anchor = NW, image = self.hero)
+
+class Entity(object):
+    def __init__(self):
+        pass
+
+    def move(self, dx, dy):
+        canvas.move(hero.entity_img, dx, dy )
+
+entities = Entity()
+def moving(e):
+    if (e.keysym == 'Up'):
+        entities.move(0,-72)
+    elif (e.keysym == 'Down'):
+        entities.move(0,72)
+    elif (e.keysym == 'Right'):
+        entities.move(72,0)            
+    elif (e.keysym == 'Left'):
+        entities.move(-72,0)
 
 canvas.pack()
 map_draw = DrawMap()
-entity_draw = DrawEntity()
+hero = DrawEntity()
+root.bind("<KeyPress>", moving)
 canvas.focus_set()
 
 root.mainloop()
