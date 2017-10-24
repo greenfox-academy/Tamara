e5aa7a1903dc48b42cf0a5ebac2baf5f5560973e
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-24 12:07:20 +0200

    feat(define_basic_info) exercise is completed

---
+++ week-07/day-01/define_basic_info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Tamara';
+let age = 31;
+let height = 1.66;
+let marriage = true;
+
+console.log('My name is ' + name + '. I\'m ' + age + ' years old and my height is ' + height + ' meter. My marriage is: ' + marriage);
