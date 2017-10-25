9c6794a9db46211cc9b9e37c26c7d14544a1aea7
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 17:07:46 +0200

    feat(string02) exercise works

---
+++ week-07/day-02/string02.js
@@ -0,0 +1,10 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+reversed = reversed.split(""); 
+reversed = reversed.reverse(); 
+reversed = reversed.join("");
+
+console.log(reversed)
