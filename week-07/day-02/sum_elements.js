532dfb1c6e9137ea4646c7ca9b0457d7ba2f6fa4
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 14:03:34 +0200

    feat(sum_elements) exercise works

---
+++ week-07/day-02/sum_elements.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+let r = [54, 23, 66, 12]
+
+function sum () {
+  let total_sum = r[1] + r[2];
+  return total_sum;
+}
+
+console.log(sum())
