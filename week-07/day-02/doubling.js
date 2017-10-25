e07e05dd055085f48fac11ac9dbc22b73beb1299
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 11:39:33 +0200

    feat(doubling) creating function for duplicating

---
+++ week-07/day-02/doubling.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+let ak = 123
+
+function doubling (ak) {
+  ak *= 2;
+  return ak
+}
+
+console.log(doubling(ak))
+
