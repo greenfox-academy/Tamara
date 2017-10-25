18a35752cc2db0fa16c571fc568a7b68e5978706
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 13:52:35 +0200

    feat(third) exercise works

---
+++ week-07/day-02/third.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `magicNumbers`
+//   with the following content: `[1, 3, 5, 7]`
+// - Print the third element of `magicNumbers`
+
+let magicNumbers = [1, 3, 5, 7]
+
+function print () {
+  console.log(magicNumbers[2])
+}
+
+print()
