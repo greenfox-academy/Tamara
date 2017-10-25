19d6e7e418a983ba9a9ff771423de7aa15557e46
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 06:19:07 +0200

    feat(draw_triangle) exercise works

---
+++ week-07/day-01/draw_triangle.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let lineCount = 4;
+
+// Write a program that draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+
+for (let i = 1; i <= lineCount; i++) {
+  console.log(Array(i+1).join("*"));
+}
