9a1dd87c040a7c00d4be190d16a9087b79ef1a46
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-25 09:59:45 +0200

    feat(draw_diagonal) modify the for loop

--- week-07/day-01/draw_diagonal.js
+++ week-07/day-01/draw_diagonal.js
@@ -1,5 +1,29 @@
 'use strict';
 
+var lineCount = 6;
+
+
+// Write a program that draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %   %
+// %%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (let i = 0; i <= lineCount-1; i++) {
+  if (i === 0 || i === lineCount-1) {
+    console.log(Array(lineCount+1).join("%"))
+  } else {
+    console.log("%" + Array(i).join(" ") + "%" + Array(lineCount-1-i).join(" ") + "%");
+  }
+}'use strict';
+
 var lineCount = 4;
 
 
