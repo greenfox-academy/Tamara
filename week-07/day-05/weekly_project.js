f01486012e5792d6c95342cd239e2d188be84988
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-27 11:49:22 +0200

    feat(weekly_project): add objects for using pictures

---
+++ week-07/day-05/weekly_project.js
@@ -0,0 +1,15 @@
+'use strict'
+
+images = [{title: 'Mountain mirror', description:'You can see a beautiful picture', url: ('https://images.fineartamerica.com/images-medium-large-5/autumn-mountain-mirror-pond-original-fine-art-photography-jerry-cowart.jpg')}, 
+  {title: 'Mountain', description: 'You can see a beautiful picture', url: ('https://www.thephotoargus.com/wp-content/uploads/2010/09/autumn-photos-16.jpg')}, 
+  {title: 'Mountain', description:'You can see a beautiful picture', url: ('https://icons.wxug.com/data/wximagenew/r/reddogg/41.jpg')}, 
+  {title: 'Mountain', description:'You can see a beautiful picture', url: ('http://www.lovethesepics.com/wp-content/uploads/2013/10/Autumn-in-Tennessee-Fall-Creek-Great-Smoky-Mountains.jpg')}, 
+  {title: 'Mountain', description:'You can see a beautiful picture', url: ('https://www.walldevil.com/wallpapers/a55/autumn-mountain-tree-virginia-usa.jpg')}]
+
+
+/* 
+('https://images.fineartamerica.com/images-medium-large-5/autumn-mountain-mirror-pond-original-fine-art-photography-jerry-cowart.jpg')
+('https://www.thephotoargus.com/wp-content/uploads/2010/09/autumn-photos-16.jpg')
+('https://icons.wxug.com/data/wximagenew/r/reddogg/41.jpg')
+('http://www.lovethesepics.com/wp-content/uploads/2013/10/Autumn-in-Tennessee-Fall-Creek-Great-Smoky-Mountains.jpg')
+('https://www.walldevil.com/wallpapers/a55/autumn-mountain-tree-virginia-usa.jpg')  */
