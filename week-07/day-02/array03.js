0ecc1e70c7fde44e6c545af27cb35db62cf39a7d
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-29 18:31:11 +0100

    feat(array03): exercise works

---
+++ week-07/day-02/array03.js
@@ -0,0 +1,10 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "r�k", "halacsk"];
+
+for (let i = 0; i <= 4; i++) {
+  far[i] += "a"; 
+}
+
+console.log(far);
