ee006c2745fd2b9c9574de8a2b15a23b2d3e4323
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-30 10:53:41 +0100

    feat(cars): add 2 solution for 2 exercise

---
+++ week-08/day-01/cars.js
@@ -0,0 +1,68 @@
+'use strict';
+// 1st
+// Call the ride method of the volvo, with 42 as a parameter
+
+var volvo = {
+  type: "Volvo",
+  fuel: 23,
+  consumption: 0.06,
+  kms: 43000,
+  ride: function (km) {
+    this.kms += km;
+    this.fuel -= km * this.consumption;
+    console.log(this.kms);
+    console.log(this.fuel); 
+  }
+};
+
+
+volvo.ride(42)
+
+
+// 2nd
+// Call the refuel function on the ferrari object using the bind method, with
+// 52 as a parameter
+
+var ferrari = {
+  type: "Ferrari",
+  fuel: 0,
+  consumption: 0.12,
+  kms: 9000,
+  ride: function (km) {
+    this.kms += km;
+    this.fuel -= km * this.consumption; 
+  }
+};
+
+function refuel(liters) {
+  this.fuel += liters;
+}
+let newFerrari = refuel.bind(ferrari);
+console.log(newFerrari(52))
+
+
+
+
+// 3rd
+// Create a tesla object that has 3 properties
+//  - type: string
+//  - battery: number
+//  - kms: number
+//  - consunption: number
+// And a method called ride, that takes a parameter celled km,
+// and increments kms with it, then drains the battery based on the consumpltion 
+
+let tesla = {
+  type: 'string',
+  battery: 15,
+  kms: 600,
+  consunption: 5,
+  ride: function (km) {
+    this.kms += km;
+    this.battery -= this.consumption; 
+  }
+}
+
+tesla.ride(36);
+console.log(tesla.kms);
+console.log(tesla.battery);
