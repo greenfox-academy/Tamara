5189523e89324c66123f87840c32dbabcea418bf
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-30 15:34:50 +0100

    feat(sharpie): creating a new solution

--- week-08/day-01/sharpie.js
+++ week-08/day-01/sharpie.js
@@ -1,26 +1,44 @@
-class Sharpie {
-  constructor(color, width) {
-    this.color = color;
-    this.width = width;
-    this.inkAmount = 100;
-  }
-  use () {
+// class Sharpie {
+//   constructor(color, width) {
+//     this.color = color;
+//     this.width = width;
+//     this.inkAmount = 100;
+//   }
+//   use () {
+//     this.inkAmount -= this.width;
+//     return this.inkAmount;
+//   }
+// }
+// var Sharpie1 = new Sharpie ("blue", 10);
+
+// let counter = function () {
+//   let timesCounter = 0;
+//     while (Sharpie1.inkAmount > 0) {
+//       Sharpie1.use();
+//       console.log(Sharpie1.inkAmount);
+//       timesCounter +=1;
+//     } console.log(timesCounter);
+// }
+
+// counter()
+
+
+function Sharpie (color, width) {
+  this.color = color;
+  this.width = width;
+  this.inkAmount = 100;
+  this.use = function () {
     this.inkAmount -= this.width;
-    return this.inkAmount;
+    if (this.inkAmount < 0) {
+      this.inkAmount = 0;
+    }
   }
 }
-var Sharpie1 = new Sharpie ("blue", 10);
+ var Sharpie2 = new Sharpie ("green", 5)
 
-let counter = function () {
-  let timesCounter = 0;
-    while (Sharpie1.inkAmount > 0) {
-      Sharpie1.use();
-      console.log(Sharpie1.inkAmount);
-      timesCounter +=1;
-    } console.log(timesCounter);
-}
+ while (Sharpie2.inkAmount > 0) {
+  Sharpie2.use();
+  console.log(Sharpie2.inkAmount);
+ }
 
-
-
-counter()
 
