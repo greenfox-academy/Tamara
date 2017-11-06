fcdcadd08ee2031979d685086cc82263a01edfde
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-06 07:43:21 +0100

    feat(reddit_project): add content to the callback function

--- week-08/day-05/reddit_project.js
+++ week-08/day-05/reddit_project.js
@@ -1,30 +1,39 @@
 'use strict';
 
 
-var button = document.getElementById('submit')
-button.addEventListener("click", addDataToPage)
+// var button = document.getElementById('submit')
+// button.addEventListener("click", addDataToPage)
 
 
 function doRequest(callback) {
   var x = new XMLHttpRequest();
   x.open('GET', 'https://cors-anywhere.herokuapp.com/http://secure-reddit.herokuapp.com/posts');
   x.onload = function() {
-      callback(x.responseText)
+     var data = JSON.parse(x.responseText).posts;
+    callback(data);
   };
   x.send();
 }
 
 function handleData(data){
   console.log(data);
+  let blah = document.createElement('a');
+  blah.href = data.title;
+  document.body.appendChild(blah);
+
+  let blah2 = document.createElement('a');
+  blah2.href = data.title;
+  document.body.appendChild(blah2);
+
 }
 
 doRequest(handleData)
 
  
-function addDataToPage() {
-    console.log("click event");
-    var location = document.getElementById('location').value;
-    if (location !== "") {
-        getData(location, handleData);
-    }
-}
+// function addDataToPage() {
+//     console.log("click event");
+//     var submit = document.getElementById('submit').value;
+//     if (location !== "") {
+//         getData(location, handleData);
+//     }
+// }
