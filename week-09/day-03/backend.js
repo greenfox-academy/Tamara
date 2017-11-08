eac813b1e1d94a6d25cdbc78c4e7551d89edf161
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-08 12:44:21 +0100

    feat(doubling): handling the input and the empty input

--- week-09/day-03/backend.js
+++ week-09/day-03/backend.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./'));
+app.use('/assets', express.static('./assets'));
+express.json.type = "application/json";
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000);
+app.get('/doubling', function(req, res) {
+  if(req.query.input !== 0){
+  res.json({
+    "received": req.query.input*1,
+    "result": req.query.input*2
+  });
+  }else{
+    res.json({
+      "error": "Please provide an input!"
+    }); 
+  };
+});
+
+app.listen(8080);
