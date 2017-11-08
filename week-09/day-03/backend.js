2d59cb50dfb0daa16e58b9c1bdd6846ceef59325
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-08 16:33:00 +0100

    feat(appenda): creating a dinamic request

--- week-09/day-03/backend.js
+++ week-09/day-03/backend.js
@@ -39,4 +39,19 @@ app.get('/greeter', function(req, res) {
   }
 });
 
+
+app.get('/appenda/:appendable', function(req, res) {
+  console.log(req.query)
+  // if(req.query.appendable){
+    res.json({
+      'appended': req.params.appendable + "a"
+    });
+  });
+  // } else if(!req.query.appendable) {
+  //     res.json({
+  //       "appended": 'not found'
+  //     });
+  //   };
+  // });
+
 app.listen(8080);
