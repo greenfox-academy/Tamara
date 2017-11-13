2ab03395931928eda1743ca24ced94e520ad958a
tamara-0527 <szigeti.tamara@gmail.com>
2017-11-13 12:28:37 +0100

    feat(exercise01): create the best solution

--- week-10/day-01/exercise01.js
+++ week-10/day-01/exercise01.js
@@ -4,22 +4,24 @@
 // it should take one parameter: what the animal says
 // Every animal should have a method called say() that prints what the animal says
 
+// '. solution: with this solution you can't use the says function in our program for other variables.
+function says () {
+  console.log(this.sound);
+};
 
-// function says () {
-//   console.log(this.sound);
-// };
+let Animals = {
+  says
+};
 
-// let Animals = {
-//   says
-// };
+let cat = {
+  sound: 'meaow'
+};
 
-// let cat = {
-//   sound: 'meaow'
-// };
+Object.setPrototypeOf(cat, Animals);
+cat.says();
 
-// Object.setPrototypeOf(cat, Animals);
-// cat.says();
 
+// 2. solution: with this solution we use huge memory.
 const Animals = {
   init: function(says) {
     this.says = says;
@@ -32,10 +34,15 @@ const Animals = {
 const dog = Object.create(Animals);
 dog.init('wwoooofff');
 dog.sound();
-// Animals.prototype.laud = function(say) {
-//   console.log('brrrrr');
-// }
 
-// var dog = new Animals('wooof');
-// console.log(dog.laud());
+
+//3. solution: this is the best solution. it can use smaller memory and we can create with it smaller objects.
+function Animals (says) {
+  this.says = says;
+};
+Animals.prototype.speak = function () {
+  console.log(this.says);
+};
+let bear = new Animals('brrr');
+bear.speak();
 
