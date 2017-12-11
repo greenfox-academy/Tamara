33b1fc742fa7402ede3b061041e8312b7ab07861
tamara-0527 <szigeti.tamara@gmail.com>
2017-12-11 22:08:02 +0100

    feat(animals_and_legs): exercise works

--- week-02/day-01/animals_and_legs.py
+++ week-02/day-01/animals_and_legs.py
@@ -1,4 +1,15 @@
 # Write a program that asks for two integers
 # The first represents the number of chickens the farmer has
-# The seconf represents the number of pigs the farmer has
+# The second represents the number of pigs the farmer has
 # It should print how many legs all the animals have
+
+
+def sum_all_legs():
+    chicken = int(input("How many chicken have you got? "))
+    pig = int(input("How many pig have you got? "))
+    sum = chicken * 2 + pig * 4
+    return "Altogether animals have " + str(sum) + " legs."
+
+
+print(sum_all_legs())
+    
