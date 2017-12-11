71df7558fead4807731b3939f2b4992685bf12ed
tamara-0527 <szigeti.tamara@gmail.com>
2017-12-11 21:59:06 +0100

    feat(average_of_input): exercise works

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,19 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+
+def average_of_numbers():
+    x = 0
+    sum = 0
+    average = 0
+    counter = 0
+    for i in range(0, 5):
+        i = int(input("Give me a number! "))
+        sum += i
+        counter += 1
+        average = float(sum/counter)
+    print("Sum: " + str(sum) + ", Average: " + str(average))
+
+
+average_of_numbers()
