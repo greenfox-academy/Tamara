43a2c6341dbae0600b4f0ff9b13217b538788fde
tamara-0527 <szigeti.tamara@gmail.com>
2017-12-11 22:32:09 +0100

    feat(cuboid): exercise works

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,20 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+a = 10
+b = 10
+c = 10
+
+def surface_area():
+    area = (2 * a * b) + (2 * a * c) + (2 * b * c)
+    return "Surface Area: " + str(area)
+
+
+def volume():
+    volume_of_cuboid = a * b * c
+    return "Volume: " + str(volume_of_cuboid)
+
+
+print(surface_area())
+print(volume())
