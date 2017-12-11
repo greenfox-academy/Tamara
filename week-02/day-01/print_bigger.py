0f2403ca74b9cc007af79c9c99e745866f3c156e
tamara-0527 <szigeti.tamara@gmail.com>
2017-12-11 20:23:40 +0100

    feat(print_bigger): exercise works

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,11 @@
 # Write a program that asks for two numbers and prints the bigger one
+a = int(input("Give me a number! "))
+b = int(input("Give me a number! "))
+
+def bigger_number():
+    if a > b :
+        print(a)
+    elif b > a:
+        print(b)
+
+bigger_number()
