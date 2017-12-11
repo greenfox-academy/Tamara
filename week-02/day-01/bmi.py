6452a2ba4e15f1bc115a311712ab5290def6ca7a
tamara-0527 <szigeti.tamara@gmail.com>
2017-12-11 22:22:45 +0100

    feat(bmi): exercise works

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -1,4 +1,11 @@
-massInKg = 81.2
-heightInM = 1.78
+mass_in_kg = 81.2
+height_in_m = 1.78
 
 #Print the Body mass index (BMI) based on these values
+
+def calculate_bmi():
+    sum_bmi = mass_in_kg / height_in_m ** 2
+    return sum_bmi
+
+
+print(calculate_bmi())
