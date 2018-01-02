35494542da86f5cdc7a7716989c5143f7845a0ca
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-02 19:04:11 +0100

    style(bmi): add new blank line

--- week-02/day-01/bmi.py
+++ week-02/day-01/bmi.py
@@ -1,7 +1,8 @@
 mass_in_kg = 81.2
 height_in_m = 1.78
 
-#Print the Body mass index (BMI) based on these values
+# Print the Body mass index (BMI) based on these values
+
 
 def calculate_bmi():
     sum_bmi = mass_in_kg / height_in_m ** 2
