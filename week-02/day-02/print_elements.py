3dcec33149f6e371e91b5681b738500c3928422a
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-02 20:27:09 +0100

    feat(print_elements): add more solutions

--- week-02/day-02/print_elements.py
+++ week-02/day-02/print_elements.py
@@ -3,4 +3,17 @@
 # - Print all the elements of `af`
 
 af = [4, 5, 6, 7]
-print(af[0:4])
+for i in af:  # with this solution print the lements in a column
+    print(i)
+
+# or
+
+print(af)  # with this solution print the lements in a list in one line
+
+# or
+
+print_in_row = ""   # With this solution print the elements in a row
+for i in af:
+    print_in_row += str(i)
+    print_in_row += " "
+print(print_in_row)
