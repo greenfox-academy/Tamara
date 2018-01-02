3b2b43dd03a559ab236313d88cb8735af006f47f
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-02 19:31:52 +0100

    style(cuboid): add blank lines and remove whitespaces

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -1,12 +1,13 @@
 # Write a program that stores 3 sides of a cuboid as variables (doubles)
 # The program should write the surface area and volume of the cuboid like:
-# 
+#
 # Surface Area: 600
 # Volume: 1000
 
 a = 10
 b = 10
 c = 10
+
 
 def surface_area():
     area = (2 * a * b) + (2 * a * c) + (2 * b * c)
