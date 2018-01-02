928202d3dc8a8d0a65b01d0a2cd262626c9a8709
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-02 19:54:03 +0100

    style(diagonal): add blank lines

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -12,11 +12,13 @@
 # The square should have as many lines as the number was
 N = int(input("Give me a number! "))
 
+
 def square():
     for i in range(N):
         if i == 0 or i == (N-1):
             print("%" * N)
         else:
-            print("%" + (i-1)* " " + "%" + (N-2-i) * " " + "%")
+            print("%" + (i-1) * " " + "%" + (N-2-i) * " " + "%")
+
 
 square()
