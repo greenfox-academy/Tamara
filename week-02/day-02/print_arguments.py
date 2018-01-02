27f6e79681236818342f499cb4edd520cb7f1df3
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-02 20:05:07 +0100

    feat(print_arguments): exercise is solved

--- week-02/day-02/print_arguments.py
+++ week-02/day-02/print_arguments.py
@@ -2,9 +2,9 @@
 #   which prints the input parameters
 #   (can have multiple number of arguments)
 
-import sys
-argv = ['hello', 'world', 'arg3', 'arg4']
 
-def printer(argv):
-    return argv
-print (argv[0:])
+def printer(*args):
+    return args
+
+
+print(printer("Hello", "world", "print"))
