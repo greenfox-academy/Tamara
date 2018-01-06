592486257411f7936cd1d00a59964a7c35ad0e1b
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-06 21:35:15 +0100

    feat(lottery): collect all lottery numbers from csv file

--- week-03/day-01/lottery.py
+++ week-03/day-01/lottery.py
@@ -4,11 +4,16 @@ import csv
 csv_file = open('otos.csv', 'r')
 content = csv.reader(csv_file, delimiter=';')
 new_list = []
+last_five_slice = slice(-5, None)
 
-def five_most_frequented_number():
+def all_lottery_numbers():
     for row in content:
-        new_list.append(row)
+        for num in row[last_five_slice]:
+           new_list.append(num)
     return new_list
 
+# def five_most_frequented_number():
+#     for 
+# print(five_most_frequented_number())
 
-print(five_most_frequented_number())
+print(all_lottery_numbers())
