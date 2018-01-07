b9e2972539e1d068c38e8c91e8dd6a9b489862e5
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-07 22:20:50 +0100

    style(draw_diagonal): remove unused elements

--- week-07/day-01/draw_diagonal.js
+++ week-07/day-01/draw_diagonal.js
@@ -22,28 +22,4 @@ for (let i = 0; i <= lineCount-1; i++) {
   } else {
     console.log("%" + Array(i).join(" ") + "%" + Array(lineCount-1-i).join(" ") + "%");
   }
-}'use strict';
-
-var lineCount = 4;
-
-
-// Write a program that draws a
-// square like this:
-//
-//
-// %%%%%
-// %%  %
-// % % %
-// %  %%
-// %   %
-// %%%%%
-//
-// The square should have as many lines as lineCount is
-
-for (let i = 0; i <= lineCount+1; i++) {
-  if (i === 0 || i === lineCount+1) {
-    console.log(Array(lineCount+2).join("%"))
-  } else {
-    console.log("%" + Array(i).join(" ") + "%" + Array(lineCount-i).join(" ") + "%");
-  }
 }
