c87d261d1b8466d4a9f58248f6acb50ca3432b16
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-13 23:24:11 +0100

    feat(frontend): creating input field and button to the filtering

--- week-09/day-04/assets/bookstore_frontend.js
+++ week-09/day-04/assets/bookstore_frontend.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const url = 'http://localhost:8080/';
+var tableElement = document.querySelector('tbody');
+const searchButton = document.querySelector('#submit');
+const nameInput = document.querySelector('#category');
 
-function ajax (command, callback) {
+function ajax (command, url, callback) {
   var xhr = new XMLHttpRequest();
-  xhr.open(command, url + 'books', true);
+  xhr.open(command, url, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onreadystatechange = function() {
-      if(xhr.readyState == 4 && xhr.status === 200) {
+      if (xhr.readyState == 4 && xhr.status === 200) {
         console.log(xhr.responseText);
           callback(JSON.parse(xhr.responseText));
       };
@@ -17,11 +20,35 @@ function ajax (command, callback) {
 
 let renderBook = function(item) {
   item.forEach(function(item) {
-      let tempBook = document.createElement('p');
+      let tempBook = document.createElement('li');
       tempBook.innerText = item.book_name;
       document.body.appendChild(tempBook);
   });
 };
 
 
-ajax('GET', renderBook);
+let listDetails = function (response) {
+  response.forEach(function(element) {
+    const listData = '<tr><td>' + element.book_name +
+        '</td><td>' + element.aut_name +
+        '</td><td>' + element.cate_descrip +
+        '</td><td>' + element.pub_name +
+        '</td><td>' + element.book_price +
+        '</td></tr>';
+    tableElement.innerHTML += listData;
+  });
+};
+
+// let searchedBooks = function (results, callback) {
+//   ajax('GET', results, callback)
+// }
+searchButton.addEventListener('click', click)
+
+function click() {
+  console.log("click event")
+  var searchedElement = nameInput.value;
+  if (searchedElement !== "") {
+      ajax('GET', url + `books/?category=${searchedElement}`, listDetails);
+  }
+}
+ajax('GET', url + 'books/', renderBook);
