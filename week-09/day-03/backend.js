f98398fbed2aae43d366c8e91e3864544d170e71
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-14 09:14:22 +0100

    feat(backend): earlier solution

--- week-09/day-03/backend.js
+++ week-09/day-03/backend.js
@@ -1,8 +1,10 @@
+'use strict'
+
 var express = require('express');
 var app = express();
 
 app.use('/assets', express.static('./assets'));
-express.json.type = "application/json";
+express.json.type = 'application/json';
 app.use(express.json());
 
 app.get('/', function(req, res) {
@@ -10,14 +12,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/doubling', function(req, res) {
-  if(req.query.input){
+  if (req.query.input) {
     res.json({
-      "received": req.query.input*1,
-      "result": req.query.input*2
+      'received': req.query.input*1,
+      'result': req.query.input*2
     });
   } else {
       res.json({
-        "error": "Please provide an input!"
+        'error': 'Please provide an input!'
     }); 
   };
 });
@@ -25,17 +27,17 @@ app.get('/doubling', function(req, res) {
 
 app.get('/greeter', function(req, res) {
   console.log(req.query)
-  if(req.query.name && req.query.title){
+  if (req.query.name && req.query.title) {
     res.json({
-     "welcome_message": "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!"
+     'welcome_message': 'Oh, hi there ' + req.query.name + ', my dear ' + req.query.title + '!'
     });
-  } else if(req.query.name && !req.query.title) {
+  } else if (req.query.name && !req.query.title) {
     res.json({
-      "error": "Please provide a title!"
+      'error': 'Please provide a title!'
     })
   } else {
     res.json({
-      "error": "Please provide a name!"
+      'error': 'Please provide a name!'
     })
   }
 });
@@ -43,7 +45,7 @@ app.get('/greeter', function(req, res) {
 
 app.get('/appenda/:appendable', function(req, res) {
   res.json({
-    'appended': req.params.appendable + "a"
+    'appended': req.params.appendable + 'a'
   });
 });
   
@@ -51,7 +53,7 @@ app.get('/appenda/:appendable', function(req, res) {
 app.post('/dountil/:what', function(req, res) {
   if (!req.params.what) {
     res.json({
-      "error": "Please provide a number!"
+      'error': 'Please provide a number!'
     });
   } else if (req.params.what === 'sum') {
     let sum = 0;
@@ -61,7 +63,7 @@ app.post('/dountil/:what', function(req, res) {
       num--;
     }
   res.json({
-    "result": sum 
+    'result': sum 
   });
   } else if (req.params.what === 'factor') {
     let fact = 1;
@@ -71,10 +73,46 @@ app.post('/dountil/:what', function(req, res) {
       num--;
     }  
   res.json({
-    "result": fact 
+    'result': fact 
     });
   };
 });
 
 
-  app.listen(8080);
+app.post('/arrays', function(req, res) {
+  if (!req.params.what || num === 0) {
+    res.json({
+      'error': 'Please provide what to do with the numbers!'
+    });
+  } else if (req.params.what === 'sum' && num === [1, 2, 5, 10]) {
+      let sum = 0;
+      let num = req.body.until;
+      for (let i = 0; i < num.length; i++) {
+        sum += num;
+      }
+  console.log(res.json({
+    'result': sum 
+  }));
+  } else if (req.params.what === 'multiply' && num === [1, 2, 5, 10]) {
+      let multiply = 0;
+      let num = req.body.until;
+      for (let i = 0; i < num.length; i++) {
+        multiply *= num;
+      }
+  console.log(res.json({
+    'result': multiply 
+  }));
+  } else if (req.params.what === 'double' && num === [1, 2, 5, 10]) {
+      let double = [];
+      let num = req.body.until;
+      for (let i = 0; i < num.length; i++) {
+        num *= 2;
+        double.push(num);
+      }
+  console.log(res.json({
+    'result': double
+  }));
+  };
+});
+
+app.listen(8080, () => {console.log('Server is running')});
